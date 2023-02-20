cb5bcfe02263e70296c5ae90c504cbe07a5fee25
nikitasairanov <110979925+nikitasairanov@users.noreply.github.com>
2023-02-20 14:37:41 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+let time = new Date();
 let themeButton = document.querySelector('#nightModeButton');
+let themeButtonDesign = document.querySelector('#nightModeButton__inner');
+if (time.getHours() >= 20) theme.href = "dark-mode.css";
+else theme.href = "light-mode.css";
+ThemeButtonChange();
+
 themeButton.onclick = function() {
-    let theme = document.querySelector("#theme")
-    if (theme.getAttribute("href") == "light-mode.css") theme.href = "dark-mode.css"
-    else theme.href = "light-mode.css"
+    let theme = document.querySelector("#theme");
+    if (theme.getAttribute("href") == "light-mode.css") theme.href = "dark-mode.css";
+    else theme.href = "light-mode.css";
+    ThemeButtonChange();
+}
+
+function ThemeButtonChange() {
+    let theme = document.querySelector("#theme");
+    if (theme.getAttribute("href") == "light-mode.css") 
+    {
+        //themeButtonDesign.classList.add('nightModeButton__inner--light');
+        themeButtonDesign.style.margin = '0 50%';
+    }
+    //else themeButtonDesign.classList.remove('nightModeButton__inner--dark');
+    else themeButtonDesign.style.margin = '0';
 }
